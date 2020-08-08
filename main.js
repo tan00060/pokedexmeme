6fb6d94192981909b359f03e989db1d6eff25de8
Michael DESKTOP <tan00060@algonquinlive.com>
2020-08-08 19:48:25 -0400

    added next and back pokemon

--- main.js
+++ main.js
@@ -4,75 +4,121 @@ const app = {
     pokemonMoves: [],
     pokemonAbilities: [],
     pokemonTypes: [],
-    
-     init: () => {
+    currentSprite: "",
+    pokemonSprites: [],
+    currentPokemon: "",
+    API: "https://pokeapi.co/api/v2/pokemon/",
+
+    init: () => {
         console.log("test")
         document.getElementById("searchButton").addEventListener('click', app.userSearchValue);
         document.getElementById("pokemonMoves").addEventListener('click', app.getMoves);
         document.getElementById("pokemonAbilities").addEventListener('click', app.getAbilities);
-     },
+        document.getElementById("rightcross").addEventListener('click', app.nextPokemon);
+        document.getElementById("leftcross").addEventListener('click', app.backPokemon);
 
-     userSearchValue: () => {
+    },
+
+    // to do
+    // add error handler if user puts in a pokemon that does not exist.
+    userSearchValue: () => {
         let searchValue = document.getElementById("myText").value
-        app.userSearch = searchValue;
-        console.log(app.userSearch)
-        app.pickPokemon()
-     },
-
-     newSearch: () =>{
-        app.userSearch = "";
-        searchValue.value = "";
-        app.pokemonMoves = [];
-        app.pokemonAbilities = [];
+        app.userSearch = searchValue.toLowerCase().trim();
+        console.log("this is what the user is searching for " + app.userSearch)
         document.getElementById("myText").value = "";
-     },
+        app.searchPokemon()
+    },
 
-     pickPokemon: () =>{
+
+    // idk why I wrote this LOL
+    // newSearch: () => {
+    //     app.userSearch = "";
+    //     searchValue.value = "";
+    //     app.pokemonMoves = [];
+    //     app.pokemonAbilities = [];
+    //     document.getElementById("myText").value = "";
+    // },
+
+    searchPokemon: () => {
         console.log("We are looking for the pokemon now!")
 
-        fetch(`https://pokeapi.co/api/v2/pokemon/${app.userSearch}`)
+        fetch(`${app.API}${app.userSearch}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data)
-                document.getElementById("pokePic").src = data.sprites.front_default
-                document.getElementById("id").textContent = `ID: ${data.id}`
-                document.getElementById("name").textContent = `Name: ${data.name}`
-                document.getElementById("type").textContent = `Type: ${data.types[0].type.name}`
-                app.pokemonMoves = data.moves;
-                app.pokemonAbilities = data.abilities;
-                app.pokemonTypes = data.types
-
-                app.getTypes()
+                app.currentPokemon = data.id
+                app.displayPokemon()
             })
-            .catch((err) =>{
+            .catch((err) => {
                 console.log("we have hit an error!!")
             })
-     },
+    },
 
-     getTypes: () =>{
-        console.log("getting pokemon types")
-        
+    getTypes: () => {
         let newString = [];
         app.pokemonTypes.forEach(type => {
             newString.push(type.type.name)
         });
 
         newString = newString.join(', ')
-        console.log(newString)
-     },
+        document.getElementById("type").textContent = `Type: ${newString}`
 
-     getMoves: () =>{
+    },
+
+    getMoves: () => {
+
+        let stats = document.getElementById("moves");
+        let list = document.createElement("ul")
+        stats.appendChild(list)
         for (let i = 0; i < app.pokemonMoves.length; i++) {
-            console.log(app.pokemonMoves[i].move.name)          
+            console.log(app.pokemonMoves[i].move.name)
+            let moves = document.createElement("li")
+            list.appendChild(moves)
+            moves.textContent = `${app.pokemonMoves[i].move.name}`
         }
-     },
+    },
 
-     getAbilities: () =>{
-         for (let i = 0; i < app.pokemonAbilities.length; i++) {
-             console.log(app.pokemonAbilities[i].ability.name)
-         }
-     }
+    getAbilities: () => {
+        for (let i = 0; i < app.pokemonAbilities.length; i++) {
+            console.log(app.pokemonAbilities[i].ability.name)
+        }
+    },
 
+    nextPokemon: () => {
+        console.log("go forward one pokemon!")
+        app.currentPokemon = app.currentPokemon + 1
+        app.displayPokemon()
+    },
+
+    backPokemon: () => {
+            console.log("go back one pokemon")
+            app.currentPokemon = app.currentPokemon - 1
+            app.displayPokemon()
+    },
+
+    displayPokemon: () => {
+
+        fetch(`${app.API}${app.currentPokemon}`)
+            .then(response => response.json())
+            .then(data => {
+
+                document.getElementById("pokePic").src = data.sprites.front_default
+                document.getElementById("id").textContent = `ID: ${data.id}`
+                document.getElementById("name").textContent = `Name: ${data.name}`
+
+                app.pokemonMoves = data.moves;
+                app.pokemonAbilities = data.abilities;
+                app.pokemonTypes = data.types
+                app.pokemonSprites = data.sprites
+                app.currentPokemon = data.id
+
+                app.getTypes()
+            })
+            .catch((err) => {
+                console.log("we have hit an error!!")
+            })
+
+
+    }
 
 
 
